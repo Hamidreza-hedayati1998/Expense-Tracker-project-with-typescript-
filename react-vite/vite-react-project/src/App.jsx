797bca683fdb8003c1ca3b7a0095e718611be8e1
HamidrezaHe <hamidrezahedayati1998@gmail.com>
2025-03-01 13:34:07 +0330

    initial commit

--- react-vite/vite-react-project/src/App.jsx
+++ react-vite/vite-react-project/src/App.jsx
@@ -1,15 +1,22 @@
-
-
-
+/* eslint-disable no-unused-vars */
 import './App.css'
-import List from './components/List'
+import Button from './components/Button';
+import Card from './components/card';
+import List from './components/List';
+const componies=[{name:"BMW",price:33000},{name:"BENZ",price:73000}];
 function App() {
- 
+ const handelselectItem=(item)=>{
+  console.log("item",item);
+ };
   return (
-    <>
-    <List/>
-    </>
-  )
-}
+  <>
+    <label style={{color:"red"}}>Hello friends</label>
+    <Card>
+      <List items={componies} onClick={handelselectItem}  label="list of companies machine"/>
+      <Button/>
+    </Card>
+  </>
+  );
+};
 
 export default App
--- react-vite/vite-react-project/src/components/List.jsx
+++ react-vite/vite-react-project/src/components/List.jsx
@@ -1,14 +1,29 @@
 
-const List=()=>{
-    const items=["Tesla","Benz","Toyota","BYD","BMW"];
+/* eslint-disable react/prop-types */
+import './List.css'
+import { useState } from "react";
+
+const List=({items,onClick,label})=>{
+    
+    const [selectedItem,setSelectedItem]=useState(0);
+    const handelclick=(index,item)=>{
+        setSelectedItem(index);
+        onClick(item);
+    };
     return(
+    <>
+        <label style={{color:"red"}}>{label}</label>
         <ul>
-            {items.length ? items.map((item,index)=>
-                <li onClick={(e)=>console.log("click",e)} key={item}>{item}-{index}</li>
+            {items?.length ? items.map((item,index)=>
+                <li 
+                onClick={()=>handelclick(index,item)}  key={item.name} className={selectedItem===index ? "seleted-item"
+                    :""}>{item.name}
+                </li>
             
             ):(<div>There is no item</div>
             )}
         </ul>
+    </>
     )
 }
 export default List;
